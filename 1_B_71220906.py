87e501a08ce943c231a9920dff874cfff1d95092
rezon123 <118422752+rezon123@users.noreply.github.com>
2022-11-17 12:55:33 +0700

    Add files via upload

---
+++ 1_B_71220906.py
@@ -0,0 +1,5 @@
+print ("=========== ALAT PENGHITUNG LINGKARAN SENTOT ==========")
+diameter = int(input("masukkan diameter (meter)"))
+hasil = diameter * 314
+print ("jarak yang harus di tempuh :", hasil, "Meter")
+
---
+++ 2_B_71220906.py
@@ -0,0 +1,4 @@
+print ("========== INI PROGRAM SEDER HANA ==========")
+pangkat = int(input("masukkan angka yang ingin dipangkatkan:"))
+hasil = pangkat**7
+print ("angka anda telah dipangkat kan 7! anda mendapat angka: ", hasil)
